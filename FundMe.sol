b7fed09ba95f9615703887e4a5ced629fb3e0bdb
Rishit Sisodia <103501433+Rishit605@users.noreply.github.com>
2022-05-08 20:29:54 +0530

    Making the code clean.

--- FundMe.sol
+++ FundMe.sol
@@ -72,12 +72,8 @@ contract FundMe {
 
     function getPrice() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (uint80 roundId,
-        int256 answer,
-        uint256 startedAt,
-        uint256 updatedAt,
-        uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        // empty commas are for unsed variables and tells solidity to not use them
         return uint256(answer);
         // 2,529.15169988
     }
